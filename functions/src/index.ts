791728cdebe8b14a1b4495ef4bcbdaf2ec3c9da4
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-12-26 11:26:51 +0800

    Init vue

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as request from "request-promise-native";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const getBusTime = functions.https.onRequest(async (request, response) => {
+	const baseUrl = 'www.random.org/integers';
+	const queryString = '?num=100&min=1&max=100&col=5&base=10&format=html&rnd=new';
+	const result = await request.get(baseUrl + queryString);
+
+	response.send(result);
+});
